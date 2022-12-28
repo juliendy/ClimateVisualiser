81f6641ea41ada73a02c202b25b0fa63dc96d7bb
juliendy <julien.yasar@gmail.com>
2022-12-28 19:54:20 +0100

    feat: init commit (skeleton of site started)

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css'
+</script>
+
+<div class="flex flex-row justify-center">
+	<div class="w-full max-w-[1000px] card bg-base-200 m-4">
+		<div class="card-body">
+			<slot />
+		</div>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<div>
+	<div class="text-center mb-8">
+		<h1 class="text-3xl leading-relaxed">
+			Visualize climate change
+			>
+		</h1>
+	</div>
+	<div>
+		<p>
+			It is normal for weather conditions to be quite different from year to year - some years are
+			hotter, some are colder; some are wetter, some are drier. However, for many places on earth,
+			strong trends can be observed. In many cases, the trend is for temperatures to be higher on
+			average. The graphics below are designed to give you an idea of how the climate has changed to
+			this date for the place where you live.
+		</p>
+        <br />
+	</div>
+</div>
